d9daeee2891c39cc64f9d65c77456bbe8a2a2180
Rahjael <surfer.vortex@gmail.com>
2021-04-02 14:55:48 +0200

    real measurement tests

--- js/DOMManager.js
+++ js/DOMManager.js
@@ -10,6 +10,428 @@ class DOMManager {
 
   constructor() {
 
+    this.misure = [
+      {
+        rif: "Filiale",
+        width: 150,
+        height: 140,
+        num: 1
+      },
+      {
+        rif: "Corridoio Attesa cantiere",
+        width: 115,
+        height: 335,
+        num: 1
+      },
+      {
+        rif: "Modigliani",
+        width: 114,
+        height: 335,
+        num: 1
+      },
+      {
+        rif: "Reception Cant 1° finestra",
+        width: 112,
+        height: 335,
+        num: 1
+      },
+      {
+        rif: "Catalano",
+        width: 107,
+        height: 330,
+        num: 1
+      },
+      {
+        rif: "Monza",
+        width: 107,
+        height: 335,
+        num: 1
+      },
+      {
+        rif: "Ghelfi",
+        width: 107,
+        height: 105,
+        num: 1
+      },
+      {
+        rif: "Fink",
+        width: 107,
+        height: 105,
+        num: 1
+      },
+      {
+        rif: "Reception Cant 3° finestra",
+        width: 107,
+        height: 330,
+        num: 1
+      },
+      {
+        rif: "Corridoio 1A Bagno",
+        width: 104,
+        height: 325,
+        num: 1
+      },
+      {
+        rif: "Julita",
+        width: 62,
+        height: 150,
+        num: 2
+      },
+      {
+        rif: "Guelfi",
+        width: 50,
+        height: 260,
+        num: 6
+      },
+      {
+        rif: "Lambri",
+        width: 50,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Fink",
+        width: 50,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Gordini",
+        width: 50,
+        height: 260,
+        num: 4
+      },
+      {
+        rif: "Filiale",
+        width: 48,
+        height: 230,
+        num: 4
+      },
+      {
+        rif: "Michelangelo",
+        width: 48,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Benzi",
+        width: 48,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Corridoio 1A",
+        width: 48,
+        height: 255,
+        num: 2
+      },
+      {
+        rif: "Cezanne",
+        width: 47,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Corridoio lato retro scala",
+        width: 47,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Reception corridoio cant",
+        width: 47,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Viganò",
+        width: 46,
+        height: 290,
+        num: 3
+      },
+      {
+        rif: "Petrosino",
+        width: 46,
+        height: 280,
+        num: 3
+      },
+      {
+        rif: "Caravaggio",
+        width: 46,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Millo",
+        width: 46,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Fagiani",
+        width: 46,
+        height: 260,
+        num: 4
+      },
+      {
+        rif: "Giotto",
+        width: 46,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Botticelli",
+        width: 46,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Reception corridoio",
+        width: 46,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Catalano",
+        width: 45,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Monza",
+        width: 45,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Ufficio RM",
+        width: 44,
+        height: 285,
+        num: 6
+      },
+      {
+        rif: "Prestia",
+        width: 44,
+        height: 220,
+        num: 2
+      },
+      {
+        rif: "Ronchi",
+        width: 44,
+        height: 220,
+        num: 2
+      },
+      {
+        rif: "Galante",
+        width: 44,
+        height: 220,
+        num: 2
+      },
+      {
+        rif: "Cavalli",
+        width: 44,
+        height: 275,
+        num: 2
+      },
+      {
+        rif: "Eleonora",
+        width: 44,
+        height: 275,
+        num: 6
+      },
+      {
+        rif: "Aiello",
+        width: 44,
+        height: 280,
+        num: 2
+      },
+      {
+        rif: "Corridoio 1",
+        width: 44,
+        height: 220,
+        num: 2
+      },
+      {
+        rif: "Filiale",
+        width: 43,
+        height: 250,
+        num: 2
+      },
+      {
+        rif: "Pre sala formazione",
+        width: 43,
+        height: 250,
+        num: 2
+      },
+      {
+        rif: "Pre sala formazione",
+        width: 43,
+        height: 190,
+        num: 2
+      },
+      {
+        rif: "Disimpegno sala formazione",
+        width: 43,
+        height: 240,
+        num: 2
+      },
+      {
+        rif: "Sala formazione",
+        width: 43,
+        height: 240,
+        num: 4
+      },
+      {
+        rif: "Sala formazione",
+        width: 43,
+        height: 195,
+        num: 4
+      },
+      {
+        rif: "Julita",
+        width: 43,
+        height: 70,
+        num: 1
+      },
+      {
+        rif: "Renoir",
+        width: 43,
+        height: 230,
+        num: 2
+      },
+      {
+        rif: "Levati",
+        width: 43,
+        height: 200,
+        num: 4
+      },
+      {
+        rif: "Grassi",
+        width: 43,
+        height: 200,
+        num: 2
+      },
+      {
+        rif: "Picasso",
+        width: 42,
+        height: 230,
+        num: 2
+      },
+      {
+        rif: "Sonzini",
+        width: 42,
+        height: 160,
+        num: 2
+      },
+      {
+        rif: "Corridoio 3° piano",
+        width: 42,
+        height: 130,
+        num: 2
+      },
+      {
+        rif: "Corridoio 3° piano",
+        width: 42,
+        height: 225,
+        num: 2
+      },
+      {
+        rif: "Corridoio scala piccola",
+        width: 42,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Barbieri",
+        width: 42,
+        height: 225,
+        num: 4
+      },
+      {
+        rif: "Cobelli",
+        width: 42,
+        height: 130,
+        num: 2
+      },
+      {
+        rif: "Servizi",
+        width: 41,
+        height: 290,
+        num: 2
+      },
+      {
+        rif: "McFarlaine",
+        width: 41,
+        height: 260,
+        num: 4
+      },
+      {
+        rif: "Sonzini",
+        width: 41,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Monet",
+        width: 41,
+        height: 140,
+        num: 2
+      },
+      {
+        rif: "Monet",
+        width: 41,
+        height: 260,
+        num: 2
+      },
+      {
+        rif: "Assistenti",
+        width: 40,
+        height: 160,
+        num: 2
+      },
+      {
+        rif: "Bagno",
+        width: 40,
+        height: 120,
+        num: 2
+      },
+      {
+        rif: "Corridoio 2",
+        width: 40,
+        height: 210,
+        num: 2
+      },
+      {
+        rif: "Sganga",
+        width: 37,
+        height: 225,
+        num: 4
+      },
+      {
+        rif: "Pelosi",
+        width: 21,
+        height: 240,
+        num: 6
+      },
+    
+    
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Default values config
     this.hHemDefaultValue = 0;
     this.vHemSupDefaultValue = 0;
@@ -180,8 +602,34 @@ class DOMManager {
     this.vHemSupDefaultValue = document.querySelector("#default-h-hem").value;
     this.vHemInfDefaultValue = document.querySelector("#default-h-hem").value;
 
+    //this.extractMilan();
+
     console.log("InputArea primed");
   }
+
+
+
+  extractMilan() {
+    console.log(this.misure);
+
+    let misureString = this.misure.reduce( (acc, obj) => {
+      let str = "";
+
+      [...Array(obj.num)].forEach( n => {
+        str += obj.rif + "-" + String(obj.width) + "-" + String(obj.height) + "-0-0-0;";
+
+      });
+      
+      return acc+str;
+
+    }, "");
+
+    console.log(misureString);
+
+    document.querySelector("#imp-exp-textarea").value = misureString;
+  }
+
+
 
   addListenersToInputFields() {
     let inputs = document.querySelectorAll("input");
@@ -776,7 +1224,7 @@ class DOMManager {
     // Remove all input fields
     let container = document.querySelector("#input-items-container");
     container.innerHTML = "";
-    
+
     this.inputItemsCounter = 0;
 
     inputValues.forEach( valueArr => {
